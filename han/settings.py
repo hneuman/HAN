678cc378f2fd626838fc4cebb9656357ba5653fa
HERNAN NEUMAN <HERNANNEUMAN16@hotmail.com>
2016-07-27 09:13:20 -0430

    "pariendo cn los static... no los reconoce heroku"

--- han/settings.py
+++ han/settings.py
@@ -140,11 +140,11 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(BASE_DIR, "/han_app/static"),
+    os.path.join(BASE_DIR, "/han/han_app/static"),
 )
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/han/han_app/static/')
+STATIC_ROOT = os.path.join('/static/')
 print " >>>>>>>> %s <<<<< " %(BASE_DIR)
 print ">>>>>>>",STATICFILES_DIRS
 print ">>>>>>>>>>   ",BASE_DIR
