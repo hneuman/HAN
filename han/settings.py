1c3c2dcc5ce8ac8bb634ef95be053f9e1072cc30
hernan neuman <hernanneuman15@hotmail.com>
2018-08-06 21:50:09 -0300

    haciendo ligeros ajustes

--- han/settings.py
+++ han/settings.py
@@ -100,12 +100,12 @@ WSGI_APPLICATION = 'han.wsgi.application'
 #amazon
 
 #DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',  'NAME': 'd4n5h8nb8a1rip',        'USER' : 'jmvpnlqmvcutqs',        'PASSWORD' : '73L581r4N4-aip6FQPK-q3kYna',        'HOST' : 'ec2-54-225-197-30.compute-1.amazonaws.com',        'PORT' : '5432',    }}
-<<<<<<< HEAD
+#<<<<<<< HEAD
 #DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',        'NAME': 'd618uru40mf7b2',        'USER' : 'dsbffrjqpspmvw',        'PASSWORD' : 'JXIgtFYX-Opg0oV4tAjpydm7ZD',        'HOST' : 'ec2-54-204-26-8.compute-1.amazonaws.com',        'PORT' : '5432',    }}
 
 #Casa
-DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',        'NAME': 'han',        'USER' : 'django',        'PASSWORD' : 'django',        'HOST' : 'localhost',        'PORT' : '5432',    }}
-=======
+DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',        'NAME': 'han2',        'USER' : 'django',        'PASSWORD' : 'django',        'HOST' : 'localhost',        'PORT' : '5432',    }}
+##=======
 #---amazon nueva, 01-08-2016############################################################
 #DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',        'NAME': 'd618uru40mf7b2',        'USER' : 'dsbffrjqpspmvw',        'PASSWORD' : 'JXIgtFYX-Opg0oV4tAjpydm7ZD',        'HOST' : 'ec2-54-204-26-8.compute-1.amazonaws.com',        'PORT' : '5432',    }}
 ###########################################################################
@@ -116,7 +116,7 @@ DATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_ps
 DATABASES = {    'default': {   'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': 'lqjshrvo',        'USER' : 'lqjshrvo',        'PASSWORD' : 'EU_aNMq8eHnqXDvtkUb1Q9xxUvKCubcZ',        'HOST' : 'elmer-01.db.elephantsql.com', 'PORT' : '5432',    }}
 ############################################################
 
->>>>>>> 6df633af507f553442b9dc9641fd22cd7d4a5f1c
+#>>>>>>> 6df633af507f553442b9dc9641fd22cd7d4a5f1c
 
 
 #DATABASES['default'] =  dj_database_url.config("postgres://jmvpnlqmvcutqs:73L581r4N4-aip6FQPK-q3kYna@ec2-54-225-197-30.compute-1.amazonaws.com:5432/d4n5h8nb8a1rip")
